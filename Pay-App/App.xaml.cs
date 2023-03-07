b8161851000082a24e3f5fa07374f6913cfa6132
Korah06 <Korah06@DESKTOP-ENTIKTF>
2023-03-07 10:58:52 +0100

    Agregar archivos de proyecto.

---
+++ Pay-App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Pay_App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Pay-App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Pay_App;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
